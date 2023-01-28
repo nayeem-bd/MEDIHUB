0a5d08c11eb063a79da00ffe4e6e637cac8f0e07
Nayeem <nimuzzamanj@gmail.com>
2023-01-29 01:22:18 +0600

    signin and signup page design complete in pug

--- controller/viewController.js
+++ controller/viewController.js
@@ -5,3 +5,15 @@ exports.landingpage=async(req,res,next)=>{
         title:'Home Page'
     });
 }
+
+exports.signUp = async(req,res,next)=>{
+    res.status(200).render('signUp',{
+        title:'Sign Up'
+    });
+}
+
+exports.signIn = async(req,res,next)=>{
+    res.status(200).render('signIn',{
+        title:'Sign In'
+    });
+}
